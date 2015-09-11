276791de2df0e5c008d6b9d018a2fe40b500db99
Max Tedford <max.tedford@gmail.com>
2015-09-11 15:26:04 -0600

    update category show pages to display loan_requests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 20)
+    @categories = Category.all
   end
 end
