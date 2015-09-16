cf3cd31c544fec97c0965f74e4618880df1fef22
Max Tedford <max.tedford@gmail.com>
2015-09-16 07:54:46 -0600

    update paginate to only include 12 lrs per page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 20)
+    @loan_requests = @category.loan_requests.paginate(:page => params[:page], :per_page => 12)
     @categories = Category.all
   end
 end
